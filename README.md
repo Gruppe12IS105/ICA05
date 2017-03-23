34d3dfc81bedd88a89964dd09554bb08b084339c
emilr15 <emilr15@uia.no>
2017-03-23 13:13:29 +0100

    Bittesmå endringer, har skrevet litt i readme.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ICA_05
-Repository for ICA_05
+# ICA05
+
+#### Så langt har vi kommet:
+
+Husk: linje 26 hadde Println("%q", contents), for å skrive ut i et annet format må man bruke Printf("%q", contents). Koden funket egentlig helt fint. Utskriften blir i JSON-format.
---
+++ Webclient/webclient_TEST.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	//"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	//"os"
+)
+
+// NOTE: denne må startes som en goroutine for hver URL/datakilde
+// som dere skal hente inn-data fra.
+
+func doGet(url string) {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%q", contents)
+		fmt.Println("   ", response.StatusCode)
+		hdr := response.Header
+		for key, value := range hdr {
+			fmt.Println("   ", key, ":", value)
+		}
+	}
+}
+
+func main() {
+	var url = "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1"
+	doGet(url)
+}
