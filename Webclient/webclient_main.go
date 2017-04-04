f940b1d21cde491cd5f3144917518c052097d22a
emilr15 <emilr15@uia.no>
2017-04-04 19:58:49 +0200

    Småting

--- Webclient/webclient_main.go
+++ Webclient/webclient_main.go
@@ -6,7 +6,9 @@ import "./API"
   Airforce: "https://api.usa.gov/jobs/search.json?query=jobs+at+the+va&organization_ids=AF"
   Nursing: "https://api.usa.gov/jobs/search.json?query=nursing+jobs"
 	Nursing in New York: "https://api.usa.gov/jobs/search.json?query=nursing+jobs+in+ny"
-	Oljepriser: "https://www.quandl.com/api/v1/datasets/EIA/IES_5_57_6_CAN.json"
+	Statistikk over Laks i Norge = "https://data.ssb.no/api/v0/dataset/1120.json?lang=en"
+	Statistikk over Døddfall i Norge = "https://data.ssb.no/api/v0/dataset/1104.json?lang=en"
+
 */
 
 func main() {
@@ -15,6 +17,12 @@ func main() {
 	var urlNursing_NY = "https://api.usa.gov/jobs/search.json?query=nursing+jobs+in+ny"
 	var urlLakseStats = "https://data.ssb.no/api/v0/dataset/1120.json?lang=en"
 	var urlDodStats = "https://data.ssb.no/api/v0/dataset/1104.json?lang=en"
+
+	/*
+		Alle go-rutinene venter 1 sek mellom hverandre for at ikke alle dataene
+		ble skrevet ut i hverandre (opplysninger fra DodStats kunne plutselig ligge
+		midt i opplysninger om LakseInfo)
+	*/
 	go API.USANursingJobs(urlNursing)
 	go API.USANursingJobs_NY(urlNursing_NY)
 	go API.LakseInfo(urlLakseStats)
