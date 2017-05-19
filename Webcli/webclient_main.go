0f55a40eab1c5bf62de04fdd4d1ee2c0d575f852
emilr15 <emilr15@uia.no>
2017-05-19 02:08:31 +0200

    Liten endring, funker fortsatt

--- Webcli/webclient_main.go
+++ Webcli/webclient_main.go
@@ -3,20 +3,26 @@ package main
 
 import (
 	"./generell_IE"
+	"./klaer_IE"
+	"./levendeDyr_IE"
 	"./siste13mnd_IE"
 	"./vaapen_IE"
-	"./levendeDyr_IE"
-	"./klaer_IE"
 )
 
 func main() {
-//API-er
+	//API-er
 	var urlImportEksport = "http://data.ssb.no/api/v0/dataset/1136.json?lang=no"
 	var urlImportEksport13mnd = "http://data.ssb.no/api/v0/dataset/1138.json?lang=no"
 	var urlImportEksportVaapen = "http://data.ssb.no/api/v0/dataset/1178.json?lang=no"
 	var urlImportEksportKlaer = "http://data.ssb.no/api/v0/dataset/1164.json?lang=no"
 	var urlImportEksportDyr = "http://data.ssb.no/api/v0/dataset/321830.json?lang=no"
-//Kjører de andre go-filene
+	/*
+		Kjører de andre go-filene, oppdatering skjer hvert 5. minutt.
+		Alle structene er like, men klarte ikke å gjøre koden dynamisk
+		nok til å kun ha én go-fil for å hente informasjon, mellomlagre
+		og skrive ut.
+		Forklaring på kode ligger i pakken generell_IE/importEksport.go
+	*/
 	go siste13mnd_IE.HentData(urlImportEksport13mnd)
 	go vaapen_IE.HentData(urlImportEksportVaapen)
 	go levendeDyr_IE.HentData(urlImportEksportDyr)
